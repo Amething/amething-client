37c89d49d3ec10b93f5b63d9a5b3aee77cddc2d0
예준 <ohman0705@naver.com>
2022-05-26 11:23:33 +0900

    GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import GlobalStyle from "./Styles/GlobalStyle";
 
 function App() {
-  return (
-    <>
-      hello
-    </>
-  );
+   return (
+      <>
+         <GlobalStyle />
+      </>
+   );
 }
 
 export default App;
