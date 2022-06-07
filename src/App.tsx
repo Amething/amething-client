c084447732348cb3e66e70ba3cf21b6a2752a6b5
eodn <s20063@gsm.hs.kr>
2022-06-07 12:26:32 +0900

    add : page 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import GlobalStyle from "./Styles/GlobalStyle";
-import Login from "./Components/Login";
+import { Login, SplashScreen } from "./Components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-   return (
-      <>
-         <GlobalStyle />
-         <Login />
-      </>
-   );
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/splash" element={<SplashScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
